9a667f27506c34d9364bf41f8d24f96710885740
zorayrbaghdasaryan <zorayrbaghdasaryan@gmail.com>
2019-04-13 23:53:21 +0400

    CRM_Project

---
+++ CRM_Project/CRM_Project/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace CRM_Project
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
